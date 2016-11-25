9f5d58e616f989096a89b60568621849e1a5e59c
Egor Suvorov <egor.suvorov@gmail.com>
2016-11-25 18:02:23 +0300

    Interpreter: rename state' to var_get

--- src/Interpreter.ml
+++ src/Interpreter.ml
@@ -3,12 +3,12 @@ struct
 
   open Language.Expr
 
-  let rec eval state = function
+  let rec eval var_get = function
     | Const  n -> n
-    | Var    x -> state x
+    | Var    x -> var_get x
     | Binop  (op, l, r) ->
-      let lv = eval state l in
-      let rv = eval state r in
+      let lv = eval var_get l in
+      let rv = eval var_get r in
       eval_binop op lv rv
 
 end
@@ -20,30 +20,30 @@ struct
 
   let eval reader writer stmt =
     let rec eval' state stmt =
-      let state' x = List.assoc x state in
+      let var_get x = List.assoc x state in
       match stmt with
       | Skip          -> state
       | Seq    (l, r) -> eval' (eval' state l) r
-      | Assign (x, e) -> (x, Expr.eval state' e) :: state
+      | Assign (x, e) -> (x, Expr.eval var_get e) :: state
       | Write   e     ->
-        writer (Expr.eval state' e);
+        writer (Expr.eval var_get e);
         state
       | Read    x     ->
         let y = reader () in
         (x, y) :: state
       | If (e, s1, s2) ->
-        let v = Expr.eval state' e in
+        let v = Expr.eval var_get e in
         eval' state (if v <> 0 then s1 else s2)
       | While (e, s) ->
-        let v = Expr.eval state' e in
+        let v = Expr.eval var_get e in
         if v <> 0 then
           eval' (eval' state s) stmt
         else
           state
       | Until (s, e) ->
         let state = eval' state s in
-        let state' x = List.assoc x state in
-        let v = Expr.eval state' e in
+        let var_get x = List.assoc x state in
+        let v = Expr.eval var_get e in
         if v = 0 then
           eval' state stmt
         else
