13fe1766d5568c61ea93a06ad39a2ae6068e4327
Egor Suvorov <egor.suvorov@gmail.com>
2016-12-02 19:11:25 +0300

    X86: S_CALL now does not save edx because it's not used in stack machine emulator

--- src/X86.ml
+++ src/X86.ml
@@ -205,12 +205,10 @@ struct
             let (args, stack') = splitAt args_cnt stack in
             let x = allocate env stack' in
             (x::stack', List.concat [
-              [X86Push ecx;
-               X86Push edx];
+              [X86Push ecx];
               List.rev @@ List.map (fun n -> X86Push n) args;
               [X86Call l;
                X86Binop (Add, L (word_size * (List.length args)), esp);
-               X86Pop edx;
                X86Pop ecx;
                X86Binop (Mov, eax, x)]
             ])
