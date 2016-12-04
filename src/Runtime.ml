45fedbb0097be8102e116179bba7fefbdc048c98
Egor Suvorov <egor.suvorov@gmail.com>
2016-12-04 12:15:48 +0300

    Int, SM: Add builtins for strings and corresponding test

--- src/Runtime.ml
+++ src/Runtime.ml
@@ -14,6 +14,19 @@ let builtins : (string * int * (Language.Value.t list -> Language.Value.t)) list
       Printf.printf "%s\n" v;
       Int 0
   );
+  ("strmake", 2, fun [Int n; Int c] -> Str (Bytes.make n (Char.chr c)));
+  ("strset", 3, fun [Str s; Int i; Int c] -> Bytes.set s i (Char.chr c); Str s);
+  ("strget", 2, fun [Str s; Int i] -> Int (Char.code @@ Bytes.get s i));
+  ("strdup", 1, fun [Str s] -> Str (Bytes.copy s));
+  ("strcat", 2, fun [Str s1; Str s2] -> Str (Bytes.concat "" [s1; s2]));
+  ("strcmp", 2, fun [Str s1; Str s2] ->
+                    let res = Bytes.compare s1 s2 in
+                    match res with
+                    | 0 -> Int 0
+                    | _ -> Int (if res > 0 then 1 else -1)
+  );
+  ("strlen", 1, fun [Str s] -> Int (Bytes.length s));
+  ("strsub", 3, fun [Str s; Int i; Int l] -> Str (Bytes.sub s i l));
 ]
 
 let builtins_fun = List.map (fun (n, a, _) -> (FunName n, Builtin a)) builtins
